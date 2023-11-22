b836b58276b947ba3b5f17bc0b764da246cda2a6
Dongmin Ahn <eamon3481@naver.com>
2023-11-22 16:04:27 +0900

    :technologist: add lint, prettier and set husky pre-commit

---
+++ .eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  ignorePatterns: ['.next/', 'node_modules/', 'public', '.yarn', '@types'],
+  plugins: ['simple-import-sort'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': 'node',
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+
+  rules: {
+    // 'React' must be in scope when using JSX 에러 해결 (Next.js)
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // react > next > @ > a~z
+          ['^react$', '^next', '^@', '^[a-z]'],
+          // ~
+          ['^~'],
+          // `../` > './'
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$', '^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Side effect imports
+          ['^\\u0000'],
+        ],
+      },
+    ],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '_',
+        varsIgnorePattern: '_',
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'typeAlias',
+        format: ['PascalCase'],
+      },
+    ],
+
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    'react/button-has-type': 'error',
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        labelComponents: ['label'],
+        labelAttributes: ['label'],
+        controlComponents: ['StyledHiddenInput'],
+        depth: 1,
+      },
+    ],
+  },
+};
