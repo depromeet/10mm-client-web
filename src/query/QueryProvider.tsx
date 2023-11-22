810e673ec843d51b77a423a085f794366933cf39
Dongmin Ahn <eamon3481@naver.com>
2023-11-23 02:54:05 +0900

    :heavy_plus_sign: add tanstack-query@4.36

---
+++ src/query/QueryProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React, { type PropsWithChildren, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const QueryProvider = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
