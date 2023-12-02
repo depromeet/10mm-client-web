4b1405319650b9799c6969901aa872ff8e6f518f
Dongmin Ahn <eamon3481@naver.com>
2023-12-02 10:54:18 +0900

    :lipstick: 메인(로그인)페이지 스타일링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,59 @@
-import Header from '@/components/Header';
+import LogoIcon from '@/app/LogoIcon';
 import { css } from '@styled-system/css';
+import { styled } from '@styled-system/jsx';
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>
-      <Header />
-      Hello 🐼!
-    </div>
+    <main className={MainWrapperCss}>
+      <div>
+        <LogoIcon />
+        <h1 className={MainTitleCss}>하루 10분의 변화를 경험하세요.</h1>
+      </div>
+      <LoginButton>게스트 로그인</LoginButton>
+    </main>
   );
 }
+
+const MainWrapperCss = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  bgImage: 'url(/images/login_bg.png)',
+  bgPosition: 'center',
+  bgRepeat: 'no-repeat',
+  bgSize: 'cover',
+
+  width: '100%',
+  height: '100vh',
+  padding: '118px 16px 50px 16px',
+});
+
+const Subtitle18Token = {
+  lineHeight: '24px',
+  fontSize: '18px',
+  fontWeight: 600,
+};
+
+const MainTitleCss = css({
+  color: '#fff',
+  textShadow: '0px 0px 5px rgba(0, 0, 0, 0.08)',
+  marginTop: '8px',
+  ...Subtitle18Token,
+});
+
+const LoginButton = styled('button', {
+  base: {
+    width: '100%',
+    padding: '16px 0px',
+
+    borderRadius: '30px',
+    boxShadow: '0px 4px 30px 5px rgba(18, 23, 41, 0.15)',
+    background: '#fff',
+
+    fontSize: '16px',
+    fontWeight: 600,
+    color: '#191F28',
+  },
+});
