a0d8e630aa821e394de1d24ae8b4a18c1aac9455
정우병 <dkrnfls@gmail.com>
2023-12-02 17:16:31 +0900

    release 최신화 (workflow 테스트) (#24)
    * :truck: panda.config.ts 수정 및 절대경로 설정
    
    * :truck: styled-system/ src 파일안으로 변경 및 gitignore 제거
    
    * :truck: 기본 디렉토리 파일 생성
    
    * chore: vscode 설정 추가
    
    * chore: sytled-system 폴더 eslint 에서 무시
    
    * feat: layout header view 구현
    
    * feat: timer 페이지 step에 따른 title, desc 추가
    
    * feat: timer view 구현 중.
    
    * :bricks: Git workflow 추가
    
    추가했지롱
    
    * :bug: Pr 실패 수정용
    
    추가했지롱
    
    * ✨그라데이션 텍스트 만들기 (타이머 안 텍스트)
    
    애니매이션도 있음!!
    
    * :zap: Nvmrc 추가
    
    추가했는데
    
    * ✨step status 추가
    
    * ♻️ timer layout 수정
    
    * 💄 일시정지 상태이면 gray로 변경
    
    * ✨ 타이머 추가
    
    * 🐛 0초 까지만 카운트
    
    * ♻️ step to timer status 네이밍 수정
    
    * ♻️merge confilct solve
    
    * feat: maxWitdth 475px로 변경
    
    ---------
    
    Co-authored-by: Dongmin Ahn <eamon3481@naver.com>
    Co-authored-by: Dongmin Ahn <68339352+wade3420@users.noreply.github.com>
    Co-authored-by: sumi-0011 <selina2000@naver.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { css } from '../../styled-system/css';
+import { css } from '@styled-system/css';
 
 export default function Home() {
   return <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>;
---
+++ src/app/timer/useTimer.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { type StepType } from '@/app/timer/useTimerStatus';
+
+export default function useTimer(status: StepType, initSeconds = 600) {
+  const [second, setSecond] = useState(initSeconds); // 남은 시간 (단위: 초)
+
+  const formattedTime = formatMMSS(second);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (second <= 0) {
+      return;
+    }
+
+    if (status === 'progress') {
+      timer = setInterval(() => {
+        setSecond((prev) => prev - 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [second, status]);
+
+  return { formattedTime };
+}
+
+const formatMMSS = (second: number): [string, string] => {
+  const minutes = Math.floor(second / 60); // 분 계산
+  const seconds = second % 60; // 초 계산
+  const formattedMinutes = String(minutes).padStart(2, '0'); // 두 자리로 변환
+  const formattedSeconds = String(seconds).padStart(2, '0'); // 두 자리로 변환
+
+  return [formattedMinutes, formattedSeconds];
+};
