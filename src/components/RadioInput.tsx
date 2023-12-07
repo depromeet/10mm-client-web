eb73fb869f5cff0b4f2b1488f00e216036fffd73
Dongmin Ahn <eamon3481@naver.com>
2023-12-07 11:46:03 +0900

    :truck: RadioInput 컴포넌트 component 파일로 옮긴다

--- src/components/RadioInput.tsx
+++ src/components/RadioInput.tsx
@@ -1,4 +1,4 @@
-import { type InputHTMLAttributes, type PropsWithChildren } from 'react';
+import { type ChangeEvent, type InputHTMLAttributes, type PropsWithChildren } from 'react';
 import CheckCircleIcon from '@/app/select/CheckCircleIcon';
 import { css } from '@styled-system/css';
 import { flex } from '@styled-system/patterns';
@@ -7,16 +7,21 @@ const purple = '#8D96F0';
 const lightPurple = '#EFF1FF';
 const grey100 = '#F2F4F6';
 
+export interface RadioInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type' | 'onChange'> {
+  onChange?: (value: string) => void;
+}
+
 /**
- * 디자인 시스템이 확정되지 않아서 component 페이지에 넣지 않고 select 폴더에 넣었습니다.
+ * 디자인 시스템이 확정시 고도화
  */
-export default function RadioInput({
-  children,
-  ...props
-}: PropsWithChildren<Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>>) {
+export default function RadioInput({ children, onChange, ...props }: PropsWithChildren<RadioInputProps>) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange && onChange(e.target.value);
+  };
+
   return (
     <label className={labelCss}>
-      <input className={InputWrapperCss} type="radio" {...props} />
+      <input className={InputWrapperCss} type="radio" onChange={handleChange} {...props} />
       <div className={RadioInputWrapperCss}>
         <p
           className={flex({
@@ -31,24 +36,18 @@ export default function RadioInput({
   );
 }
 
-const subtitle = {
-  fontSize: '16px',
-  fontWeight: 600,
-  lineHeight: '24px',
-};
-
 const labelCss = css({
   cursor: 'pointer',
 });
 
 const RadioInputWrapperCss = flex({
+  textStyle: 'subtitle3',
   padding: '10px 16px',
+
   borderRadius: '36px',
-  ...subtitle,
   background: grey100,
 
   justifyContent: 'space-between',
-
   alignItems: 'center',
 
   transition: '0.3s ease',
@@ -66,6 +65,7 @@ const InputWrapperCss = css({
   _checked: {
     '& ~ div': {
       color: purple,
+      textStyle: 'subtitle2',
       backgroundColor: lightPurple,
       '& svg': {
         display: 'block',
