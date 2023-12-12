3db99c34cfee6d91d7d83efe5243fbb9f1c682ba
byun sumi <sumi@adenasoft.com>
2023-12-12 15:31:10 +0900

    ♻️ gradient keyframe token 정리

---
+++ src/styles/motion.ts
@@ -0,0 +1,9 @@
+import { defineKeyframes } from '@pandacss/dev';
+
+export const keyframeList = defineKeyframes({
+  gradient: {
+    '0%': { transform: 'rotate(0deg)', backgroundPositionX: '0%', backgroundPositionY: '0%' },
+    '50%': { backgroundPositionX: '50%', backgroundPositionY: '100%' },
+    '100%': { transform: 'rotate(0deg)', backgroundPositionX: '0%', backgroundPositionY: '0%' },
+  },
+});
