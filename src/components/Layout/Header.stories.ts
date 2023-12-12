cce3cb12b2af82edba02fbf839941a362595b0f5
byun sumi <sumi@adenasoft.com>
2023-12-12 15:44:18 +0900

    🐛 invariant expected app router to be mounted error fix

--- src/components/Layout/Header.stories.ts
+++ src/components/Layout/Header.stories.ts
@@ -9,6 +9,9 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
