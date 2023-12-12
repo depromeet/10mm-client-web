cf68f67a3b9ed400218cd913d6478c8e11540b6b
byun sumi <sumi@adenasoft.com>
2023-12-12 15:49:51 +0900

    ♻️ 모든곳에 적용되는 오류이므로 storybook priview에 추가

--- src/components/Layout/Header.stories.ts
+++ src/components/Layout/Header.stories.ts
@@ -9,9 +9,6 @@ const meta = {
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
-    nextjs: {
-      appDirectory: true,
-    },
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
@@ -19,6 +16,7 @@ const meta = {
 } satisfies Meta<typeof Header>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
