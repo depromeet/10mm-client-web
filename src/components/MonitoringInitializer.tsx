21d992b975c7ab129f1ffc593b4b92089f708436
junoshon <sanoldfox@naver.com>
2023-12-12 14:45:23 +0900

    :bug: GA 트래킹 ID 수정
    GA 트래킹 ID 수정

--- src/components/MonitoringInitializer.tsx
+++ src/components/MonitoringInitializer.tsx
@@ -4,27 +4,18 @@ const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 const MonitoringInitializer = () => {
   return (
-    <>
-      {
-        <>
-          <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-          <Script
-            id="gtag-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-            `,
-            }}
-          />
-        </>
-      }
-    </>
+    <div className="container">
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', ${GA_TRACKING_ID});
+        `}
+      </Script>
+    </div>
   );
 };
 
