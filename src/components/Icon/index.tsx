7dbfb356dcd24ec51a04a6c514f351fb3d73b662
Dongmin Ahn <eamon3481@naver.com>
2023-12-13 19:08:04 +0900

    :label: Icon 컴포넌트에서 ColorToken 타입을 받아서 그릴수있게 변경 & 타입 추론 편하게 IconComponentMap 에서 key 값으로 name type narrow

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,15 +1,24 @@
 import { type SVGProps } from 'react';
 import LeftArrowIcon from '@/components/Icon/LeftArrowIcon';
+import { type ColorToken } from '@styled-system/tokens';
 
-type IconType = 'left-arrow';
+const IconComponentMap = {
+  'left-arrow': LeftArrowIcon,
+} as const;
 
-interface Props extends SVGProps<SVGSVGElement> {
-  name: IconType;
+interface Props extends IconComponentProps {
+  name: keyof typeof IconComponentMap;
+  color?: ColorToken;
+}
+
+export interface IconComponentProps extends SVGProps<SVGSVGElement> {
+  color?: ColorToken;
 }
 
 export default function Icon({ name, ...props }: Props) {
-  switch (name) {
-    case 'left-arrow':
-      return <LeftArrowIcon {...props} />;
-  }
+  const IconComponent = IconComponentMap[name];
+
+  return <IconComponent {...props} />;
 }
+
+export const DEFAULT_ICON_COLOR = '#D8D8DD';
