35155b7f54e2614b2313dae0ad8c55ce6b4a946c
byun sumi <sumi@adenasoft.com>
2023-12-13 15:45:02 +0900

    ♻️ button story 적용

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,74 +1,40 @@
 import Button from '@/components/Button/Button';
 import type { Meta, StoryObj } from '@storybook/react';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const CTA: Story = {
   args: {
     size: 'large',
-    children: 'Default Large Button',
-  },
-};
-
-export const CTALarge: Story = {
-  args: {
-    size: 'large',
-    children: 'CTA Large Button',
+    children: 'CTA Button',
     variant: 'cta',
   },
 };
 
-export const PrimaryLarge: Story = {
+export const Primary: Story = {
   args: {
     size: 'large',
-    children: 'PrimaryLarge',
+    children: 'Primary Button',
     variant: 'primary',
   },
 };
 
-export const SecondaryLargeButton: Story = {
+export const SecondaryButton: Story = {
   args: {
     size: 'large',
-    children: 'SecondaryLargeButton',
-    variant: 'secondary',
-  },
-};
-
-export const CTAMedium: Story = {
-  args: {
-    size: 'medium',
-    children: 'CTA Large Button',
-    variant: 'cta',
-  },
-};
-
-export const PrimaryMedium: Story = {
-  args: {
-    size: 'medium',
-    children: 'PrimaryMedium',
-    variant: 'primary',
-  },
-};
-
-export const SecondaryMediumButton: Story = {
-  args: {
-    size: 'medium',
-    children: 'SecondaryMediumButton',
+    children: 'Secondary Button',
     variant: 'secondary',
   },
 };
