db31892d47fbc97f65cd2c52d90b9198f000ab79
Dongmin Ahn <eamon3481@naver.com>
2023-12-15 04:05:49 +0900

    :sparkles: DefaultDialog 컴포넌트 추가

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,16 @@
+import DefaultDialog from '@/components/Dialog/DefaultDialog';
+import { type DialogProps } from '@/components/Dialog/Dialog.types';
+import { type ModalProps } from '@/components/Modal/Modal';
+
+/**
+ * dialog 컴포넌트
+ * @description 각 dialog variant 타입에 따라 다른 컴포넌트들을 랜더링합니다.
+ * @param variant 'default' | 'list' | 'select'
+ */
+function Dialog(props: DialogProps & ModalProps) {
+  if (props.variant === 'default') {
+    return <DefaultDialog {...props} />;
+  }
+}
+
+export default Dialog;
---
+++ src/components/Dialog/Dialog.types.ts
@@ -0,0 +1,35 @@
+import { type ModalProps } from '@/components/Modal/Modal';
+
+interface ButtonGroupProps {
+  confirmText?: string;
+  onConfirm?: () => void;
+  cancelText?: string;
+  onCancel?: () => void;
+}
+
+type Value = {
+  value: string;
+  text: string;
+};
+
+export interface DefaultDialogProps extends ButtonGroupProps, ModalProps {
+  variant: 'default';
+  title?: string;
+  content?: string;
+}
+
+export interface ListDialogProps extends ModalProps {
+  variant: 'list';
+  title: string;
+  list: Value[];
+  onClick: (value: Value) => void;
+}
+
+export interface SelectDialogProps extends Omit<ButtonGroupProps, 'onConfirm'>, ModalProps {
+  variant: 'select';
+  title?: string;
+  content?: string;
+  selects: Value[];
+  onConfirm: (value: Value) => void;
+}
+export type DialogProps = DefaultDialogProps | ListDialogProps | SelectDialogProps;
