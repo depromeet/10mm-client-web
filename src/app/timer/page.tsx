d232e5598cc07a7349e50a3d6208ddf9b24829df
sumi-0011 <selina2000@naver.com>
2023-12-15 01:34:45 +0900

    ✨ button 수정된 컴포넌트 적용

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -2,6 +2,7 @@
 import { MISSION_CATEGORIES } from '@/app/select/select.constants';
 import useTimer from '@/app/timer/useTimer';
 import useTimerStatus from '@/app/timer/useTimerStatus';
+import Button from '@/components/Button/Button';
 import Stopwatch from '@/components/Stopwatch/Stopwatch';
 import useSearchParamsTypedValue from '@/hooks/useSearchParamsTypedValue';
 import { type ObjectKeys } from '@/utils';
@@ -26,76 +27,36 @@ export default function TimerPage() {
       <p className={descCss}>{stepLabel.desc}</p>
 
       <section className={stopwatchCss}>
-        <Stopwatch minutes={minutes} seconds={seconds} category={category} stack={1} />
+        <Stopwatch minutes={minutes} seconds={seconds} category={category} stack={1} isDisabled={step === 'stop'} />
       </section>
       <section className={buttonContainerCss}>
         {step === 'ready' && (
-          <button type="button" onClick={() => onNextStep('progress')}>
+          <Button variant="cta" size="large" type="button" onClick={() => onNextStep('progress')}>
             시작
-          </button>
+          </Button>
         )}
         {step === 'progress' && (
           <>
-            <button type="button" onClick={() => onNextStep('stop')}>
+            <Button size="medium" variant="secondary" type="button" onClick={() => onNextStep('stop')}>
               일시정지
-            </button>
-            <button type="button" onClick={onFinish}>
+            </Button>
+            <Button size="medium" variant="primary" type="button" onClick={onFinish}>
               끝내기
-            </button>
+            </Button>
           </>
         )}
         {step === 'stop' && (
           <>
-            <button type="button" onClick={() => onNextStep('progress')}>
+            <Button size="medium" variant="secondary" type="button" onClick={() => onNextStep('progress')}>
               다시 시작
-            </button>
-            <button type="button" onClick={onFinish}>
+            </Button>
+            <Button size="medium" variant="secondary" type="button" onClick={onFinish}>
               끝내기
-            </button>
+            </Button>
           </>
         )}
       </section>
     </div>
-    // <div
-    //   className={css(bgCss, {
-    //     background: step === 'stop' ? '#F2F4F6' : 'linear-gradient(136deg, #FFF1F2 4.76%, #E9EFFF 89.58%)',
-    //   })}
-    // >
-    //   <Header title={'미션 타이머'} />
-    //   <div className={css(containerCss)}>
-    //     <h1 className={titleCss}>{stepLabel.title}</h1>
-    //     <p className={descCss}>{stepLabel.desc}</p>
-
-    //     <TimerView category={category} time={formattedTime} isActive={step !== 'stop'} />
-    //     <div className={css(buttonContainerCss)}>
-    //       {step === 'ready' && (
-    //         <button type="button" onClick={() => onNextStep('progress')}>
-    //           시작
-    //         </button>
-    //       )}
-    //       {step === 'progress' && (
-    //         <>
-    //           <button type="button" onClick={() => onNextStep('stop')}>
-    //             일시정지
-    //           </button>
-    //           <button type="button" onClick={onFinish}>
-    //             끝내기
-    //           </button>
-    //         </>
-    //       )}
-    //       {step === 'stop' && (
-    //         <>
-    //           <button type="button" onClick={() => onNextStep('progress')}>
-    //             다시 시작
-    //           </button>
-    //           <button type="button" onClick={onFinish}>
-    //             끝내기
-    //           </button>
-    //         </>
-    //       )}
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
@@ -116,11 +77,4 @@ const buttonContainerCss = css({
   display: 'flex',
   justifyContent: 'center',
   gap: '12px',
-
-  '& button': {
-    backgroundColor: 'white',
-    borderRadius: '30px',
-    padding: '16px 24px',
-    boxSizing: '0px 4px 20px 0px rgba(18, 23, 41, 0.10)',
-  },
 });
