d5dea9d5a97152c89e9beafabba1360448a6ad9b
Dongmin Ahn <eamon3481@naver.com>
2023-12-20 18:59:09 +0900

    :bug: 올바른 gtag 입력을 위한 fix

--- src/components/MonitoringInitializer.tsx
+++ src/components/MonitoringInitializer.tsx
@@ -12,7 +12,7 @@ const MonitoringInitializer = () => {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', ${GA_TRACKING_ID});
+          gtag('config', '${GA_TRACKING_ID}');
         `}
       </Script>
     </div>
