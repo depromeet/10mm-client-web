4b63bf09df53223afaa7ccd6357d6a32dcd09fac
Dongmin Ahn <eamon3481@naver.com>
2023-12-22 10:49:32 +0900

    :sparkles: 로깅 log 변경

--- src/utils/event/index.ts
+++ src/utils/event/index.ts
@@ -5,7 +5,7 @@ import gtag from './gtag';
 
 export const logEvent = (eventName: string, label: string, properties?: Record<string, string>) => {
   if (!isProd()) {
-    console.log(`Event: ${eventName}, Label: ${label}, Properties: ${JSON.stringify(properties)}`); // eslint-disable-line no-console
+    console.log('EVENT LOGGING', { eventName, label, properties }); // eslint-disable-line no-console
     return;
   }
   gtag.event({ action: eventName, label, params: { ...properties } });
