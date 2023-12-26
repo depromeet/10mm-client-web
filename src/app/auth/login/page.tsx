e306597a710534c2092a49692c4db7b017d74c38
wooBottle <dkrnfls@gmail.com>
2023-12-27 00:48:49 +0900

    :art: 둘러보기 액션 추가

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import Button from '@/components/Button/Button';
 import ButtonSocialLogin from '@/components/ButtonSocialLogin/ButtonSocialLogin';
+import { ROUTER } from '@/constants/router';
 import { css } from '@styled-system/css';
 
 export default function LoginPage() {
+  const router = useRouter();
+  const onClickGuest = () => {
+    router.push(ROUTER.GUEST.MISSION.NEW);
+  };
+
   return (
     <main className={MainWrapperCss}>
       <div className={LoginButtonListWrapperCss}>
         <ButtonSocialLogin type="kakao" />
         <ButtonSocialLogin type="apple" />
-        <Button size="large" variant="ghost">
+        <Button size="large" variant="ghost" onClick={onClickGuest}>
           둘러보기
         </Button>
       </div>
