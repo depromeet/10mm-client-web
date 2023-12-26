f464d6f9337aadac19b0b7f9d646aca276a0f833
wooBottle <dkrnfls@gmail.com>
2023-12-26 16:12:23 +0900

    :art: ApiInstance 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,3 +1,13 @@
 // axios의 인스턴스 구현체가 있고 전반적으로 instance를 이용해서 api 콜하는 함수들
 
-export {};
+import apiInstance from './instance.api';
+
+const APIS = {
+  // 아래는 예시
+  getPosts: async () => {
+    const { data } = await apiInstance.get('/posts');
+    return data;
+  },
+};
+
+export default APIS;
