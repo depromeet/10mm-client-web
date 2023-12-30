32151503b842868e9ca75113cc105490dc682ebd
sumi-0011 <selina2000@naver.com>
2023-12-30 21:19:01 +0900

    ✨ 홈 header 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import AppBar from '@/app/home/AppBar';
 import LogoIcon from '@/app/LogoIcon';
 import { ROUTER } from '@/constants/router';
 import { eventLogger } from '@/utils';
@@ -8,13 +9,14 @@ import { css } from '@styled-system/css';
 import { flex } from '@styled-system/patterns';
 
 export default function Home() {
-  const handleLogin = () => {
-    eventLogger.logEvent('click/guestLogin', 'guest_login');
-  };
+  // const handleLogin = () => {
+  //   eventLogger.logEvent('click/guestLogin', 'guest_login');
+  // };
 
   return (
-    <main className={MainWrapperCss}>
-      <div className={logoCss}>
+    <main className={mainCss}>
+      <AppBar />
+      {/* <div className={logoCss}>
         <LogoIcon />
         <h1 className={MainTitleCss}>하루 10분의 변화를 경험하세요.</h1>
       </div>
@@ -22,50 +24,11 @@ export default function Home() {
         <button type={'button'} className={LoginButtonCss}>
           게스트 로그인
         </button>
-      </Link>
+      </Link> */}
     </main>
   );
 }
 
-const logoCss = flex({
-  flexDirection: 'column',
-  alignItems: 'center',
-});
-
-const MainWrapperCss = css({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-
-  bgImage: 'url(/images/login_bg.png)',
-  bgPosition: 'center',
-  bgRepeat: 'no-repeat',
-  bgSize: 'cover',
-
-  width: '100%',
-  height: '100vh',
-  padding: '118px 16px 50px 16px',
-});
-
-const MainTitleCss = css({
-  color: '#fff',
-  textShadow: '0px 0px 5px rgba(0, 0, 0, 0.08)',
-  marginTop: '8px',
-  textStyle: 'subtitle1',
-});
-
-const LoginButtonCss = css({
-  width: '100%',
-  padding: '16px 0px',
-  cursor: 'pointer',
-
-  borderRadius: '16px',
-  boxShadow: '0px 4px 30px 5px rgba(18, 23, 41, 0.15)',
-  background: 'basicColor.white',
-  textStyle: 'subtitle2',
-});
-
-const LinkCss = css({
-  width: '100%',
+const mainCss = css({
+  minHeight: '100vh',
 });
