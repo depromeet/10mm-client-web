bf892529927d3686e6d32ced340b78983da9de50
sumi-0011 <selina2000@naver.com>
2023-12-31 17:21:35 +0900

    ✨ 버전 정보 & appbar-bottom 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,15 +1,19 @@
 import MenuList from '@/app/mypage/MenuList';
 import Profile from '@/app/mypage/Profile';
+import VersionInfo from '@/app/mypage/VersionInfo';
+import AppBarBottom from '@/components/AppBarBottom/AppBarBottom';
 import Header from '@/components/Header/Header';
 
 function Mypage() {
   const nickname = '수미칩';
   return (
-    <div>
+    <main>
       <Header isBackIcon={false} title="마이페이지" rightAction="none" />
       <Profile nickname={nickname} />
       <MenuList />
-    </div>
+      <VersionInfo />
+      <AppBarBottom />
+    </main>
   );
 }
 
