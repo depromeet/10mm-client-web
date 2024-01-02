a0a46f76d70e5fc812a71544233fef469c2b3fa4
Dongmin Ahn <eamon3481@naver.com>
2024-01-02 23:54:08 +0900

    :lipstick: text button 인경우 피그마와 다르게 구현된 부분 수정

--- src/components/Header/IconHeader.tsx
+++ src/components/Header/IconHeader.tsx
@@ -1,12 +1,17 @@
 import { type IconHeaderType } from '@/components/Header/Header.types';
 import HeaderBase from '@/components/Header/HeaderBase';
 import Icon from '@/components/Icon';
+import { css } from '@styled-system/css';
 
 function IconHeader({ iconName = 'close', ...props }: IconHeaderType) {
   return (
     <HeaderBase
       rightElement={
-        <div>
+        <div
+          className={css({
+            padding: '12px',
+          })}
+        >
           <Icon name={iconName} width={20} height={20} />
         </div>
       }
