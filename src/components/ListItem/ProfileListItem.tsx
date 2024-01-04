9faa643600099dccf13a684b322f3f52bebdc875
sumi-0011 <selina2000@naver.com>
2024-01-04 23:11:32 +0900

    🎨 list/profile 컴포넌트 구현

--- src/components/ListItem/ProfileListItem.tsx
+++ src/components/ListItem/ProfileListItem.tsx
@@ -1,13 +1,34 @@
+import { type MouseEventHandler } from 'react';
 import Button from '@/components/Button/Button';
-import { type ProfileListItemType } from '@/components/ListItem/ListItem.types';
-import { css } from '@/styled-system/css';
+import { oneLineTextCss } from '@/components/ListItem/ListItem.styles';
+import Thumbnail from '@/components/Thumbnail/Thumbnail';
+import { type ThumbnailProps } from '@/components/Thumbnail/Thumbnail.types';
+import { css, cx } from '@/styled-system/css';
 import { flex } from '@/styled-system/patterns';
 
-function ProfileListItem(props: ProfileListItemType) {
+interface Props {
+  thumbnail?: ThumbnailProps;
+  name: string;
+  onButtonClick?: MouseEventHandler<HTMLButtonElement>;
+
+  variant?: 'one-button' | 'two-button';
+  onSubButtonClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+function ProfileListItem(props: Props) {
+  const isExistFollowerButton = props.variant === 'two-button';
+
   return (
     <li className={containerCss}>
-      {props.thumbnailElement}
-      <p className={nameCss}>{props.name}</p>
+      <Thumbnail size="h36" {...props.thumbnail} />
+      <p className={cx(nameCss, oneLineTextCss, isExistFollowerButton && existFollowerButtonCss)}>
+        {props.name}
+        {isExistFollowerButton && (
+          <span className={followLabelCss} onClick={props.onSubButtonClick}>
+            팔로우
+          </span>
+        )}
+      </p>
       {props.onButtonClick && (
         <Button size="small" variant="primary" onClick={props.onButtonClick} className={buttonCss}>
           팔로우
@@ -25,11 +46,21 @@ const nameCss = css({
   flex: 1,
   color: 'text.secondary',
   textStyle: 'subtitle4',
-  whiteSpace: 'nowrap',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
+  position: 'relative',
+});
+
+const existFollowerButtonCss = css({
+  paddingRight: '48px',
 });
 
 const buttonCss = css({
   marginLeft: '4px',
+});
+
+const followLabelCss = css({
+  top: 0,
+  right: 0,
+  position: 'absolute',
+  color: 'purple.purple600',
+  textStyle: 'subtitle4',
 });
