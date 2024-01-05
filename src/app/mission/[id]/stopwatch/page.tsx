5e6386e2eb9ac79aaaa11bd67c92162c019babb2
sumi-0011 <selina2000@naver.com>
2024-01-06 01:11:58 +0900

    🎨 다양한 모달 컴포넌트 생성 및 구조 변경

--- src/app/mission/[id]/stopwatch/page.tsx
+++ src/app/mission/[id]/stopwatch/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { BackDialog, FinalDialog, MidOutDialog } from '@/app/mission/[id]/stopwatch/modals';
 import Button from '@/components/Button/Button';
-import Dialog from '@/components/Dialog/Dialog';
+import Header from '@/components/Header/Header';
 import Stopwatch from '@/components/Stopwatch/Stopwatch';
 import { ROUTER } from '@/constants/router';
 import useStopwatch from '@/hooks/mission/stopwatch/useStopwatch';
@@ -19,20 +20,30 @@ export default function StopwatchPage() {
   const { step, prevStep, stepLabel, onNextStep } = useStopwatchStatus();
   const { seconds, minutes, stepper } = useStopwatch(step);
 
-  const { isOpen, openModal, closeModal } = useModal();
+  const logData = {
+    category,
+    finishTime: Number(minutes) * 60 + Number(seconds),
+  };
+
+  const { isOpen: isFinalOpen, openModal: openFinalModal, closeModal: closeFinalModal } = useModal();
+  const { isOpen: isBackOpen, openModal: openBackModal, closeModal: closeBackModal } = useModal();
+  const { isOpen: isMidOutOpen, openModal: openMidOutModal, closeModal: closeMidOutModal } = useModal();
 
   const onFinishButtonClick = () => {
     eventLogger.logEvent('click/finishButton', 'stopwatch', { category });
-    openModal();
+    openFinalModal();
     onNextStep('stop');
   };
 
+  const onExit = () => {
+    // 강제 이탈
+    // TODO : id 연결
+    const DUMMY_ID = '1';
+    router.push(ROUTER.MISSION.DETAIL(DUMMY_ID));
+  };
+
   const onFinish = () => {
-    eventLogger.logEvent('click/finish', 'stopwatch', {
-      category,
-      finishTime: Number(minutes) * 60 + Number(seconds),
-    });
-    // TODO: 끝내기 후 로직 추가
+    // TODO: 끝내기 로직 추가
     router.push(ROUTER.MISSION.SUCCESS);
   };
 
@@ -55,70 +66,72 @@ export default function StopwatchPage() {
   };
 
   return (
-    <div className={containerCss}>
-      <h1 className={titleCss}>{stepLabel.title}</h1>
-      <p className={descCss}>{stepLabel.desc}</p>
+    <>
+      <Header rightAction="none" onBackAction={openBackModal} />
+      <div className={containerCss}>
+        <h1 className={titleCss}>{stepLabel.title}</h1>
+        <p className={descCss}>{stepLabel.desc}</p>
 
-      <section className={stopwatchCss}>
-        <Stopwatch
-          minutes={minutes}
-          seconds={seconds}
-          category={category}
-          stepper={stepper}
-          isProgress={step === 'progress'}
-          isDisabled={step === 'stop'}
+        <section className={stopwatchCss}>
+          <Stopwatch
+            minutes={minutes}
+            seconds={seconds}
+            category={category}
+            stepper={stepper}
+            isProgress={step === 'progress'}
+            isDisabled={step === 'stop'}
+          />
+        </section>
+        <section className={buttonContainerCss}>
+          {step === 'ready' && (
+            <Button variant="cta" size="large" type="button" onClick={onStart}>
+              시작
+            </Button>
+          )}
+          {step === 'progress' && (
+            <>
+              <Button size="medium" variant="secondary" type="button" onClick={onStop}>
+                일시 정지
+              </Button>
+              <Button size="medium" variant="primary" type="button" onClick={onFinishButtonClick}>
+                끝내기
+              </Button>
+            </>
+          )}
+          {step === 'stop' && (
+            <>
+              <Button size="medium" variant="secondary" type="button" onClick={() => onNextStep('progress')}>
+                다시 시작
+              </Button>
+              <Button size="medium" variant="primary" type="button" onClick={onFinishButtonClick}>
+                끝내기
+              </Button>
+            </>
+          )}
+        </section>
+        <FinalDialog
+          isOpen={isFinalOpen}
+          onClose={closeFinalModal}
+          onCancel={onCancel}
+          onConfirm={onFinish}
+          logData={logData}
         />
-      </section>
-      <section className={buttonContainerCss}>
-        {step === 'ready' && (
-          <Button variant="cta" size="large" type="button" onClick={onStart}>
-            시작
-          </Button>
-        )}
-        {step === 'progress' && (
-          <>
-            <Button size="medium" variant="secondary" type="button" onClick={onStop}>
-              일시 정지
-            </Button>
-            <Button size="medium" variant="primary" type="button" onClick={onFinishButtonClick}>
-              끝내기
-            </Button>
-          </>
-        )}
-        {step === 'stop' && (
-          <>
-            <Button size="medium" variant="secondary" type="button" onClick={() => onNextStep('progress')}>
-              다시 시작
-            </Button>
-            <Button size="medium" variant="primary" type="button" onClick={onFinishButtonClick}>
-              끝내기
-            </Button>
-          </>
-        )}
-      </section>
-      <FinalDialog isOpen={isOpen} onClose={closeModal} onCancel={onCancel} onAction={onFinish} />
-    </div>
-  );
-}
-
-function FinalDialog(props: {
-  isOpen: boolean;
-  onClose: VoidFunction;
-  onAction: VoidFunction;
-  onCancel: VoidFunction;
-}) {
-  return (
-    <Dialog
-      variant={'default'}
-      title="미션을 끝내시겠어요?"
-      content="지금까지 집중한 시간들이 기록됩니다."
-      confirmText="끝내기"
-      cancelText="취소"
-      isOpen={props.isOpen}
-      onClose={props.onClose}
-      onCancel={props.onCancel}
-      onConfirm={props.onAction}
-    />
+        <BackDialog
+          isOpen={isBackOpen}
+          onClose={closeBackModal}
+          onCancel={onCancel}
+          onConfirm={onExit}
+          logData={logData}
+        />
+        <MidOutDialog
+          isOpen={isMidOutOpen}
+          onClose={closeMidOutModal}
+          onCancel={onCancel}
+          onConfirm={onExit}
+          logData={logData}
+        />
+      </div>
+    </>
   );
 }
 
