bd272a4f83e0c9b695ef3b743ef766a624933401
sumi-0011 <selina2000@naver.com>
2024-01-06 01:12:08 +0900

    🎨 bg color fix

--- src/app/mission/[id]/stopwatch/layout.tsx
+++ src/app/mission/[id]/stopwatch/layout.tsx
@@ -1,17 +1,11 @@
 import { type PropsWithChildren } from 'react';
-import Header from '@/components/Header/Header';
 import { css } from '@/styled-system/css';
 
 export default function Layout({ children }: PropsWithChildren) {
-  return (
-    <div className={containerCss}>
-      <Header rightAction="none" />
-
-      {children}
-    </div>
-  );
+  return <div className={containerCss}>{children}</div>;
 }
 
 const containerCss = css({
   minHeight: '100vh',
+  backgroundColor: 'bg.surface1',
 });
