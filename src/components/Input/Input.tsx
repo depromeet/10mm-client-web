d99f0638963a2fb005866e7cc7e33f4eef514c1b
junoshon <sanoldfox@naver.com>
2024-01-08 19:24:35 +0900

    :sparkles: Input variant에 normal-button 케이스 추가
    input variant에 normal-button 케이스 추가

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,10 +2,14 @@ import DropdownInput from '@/components/Input/DropdownInput';
 import { type InputType } from '@/components/Input/Input.types';
 import NormalInput from '@/components/Input/NormalInput';
 
+import NormalButtonInput from './NormalButtonInput';
+
 function Input<Value extends string = string>(props: InputType<Value>) {
   switch (props.variant) {
     case 'drop-down':
       return <DropdownInput {...props} />;
+    case 'normal-button':
+      return <NormalButtonInput {...props} />;
     case 'normal':
     default:
       return <NormalInput {...props} />;
