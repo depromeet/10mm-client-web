995d473842fd14b7e98ed0025741edbb1f5b4371
Dongmin Ahn <eamon3481@naver.com>
2024-01-12 15:25:59 +0900

    :sparkles: deploy 환경에서 msw 실행

--- src/msw/MSWInitComponent.tsx
+++ src/msw/MSWInitComponent.tsx
@@ -1,19 +1,19 @@
 'use client';
 
 const mswInit = () => {
-  if (process.env.NODE_ENV === 'development') {
-    if (typeof window === 'undefined') {
-      (async () => {
-        const { server } = await import('./node');
-        server.listen();
-      })();
-    } else {
-      (async () => {
-        const { worker } = await import('./browser');
-        worker.start();
-      })();
-    }
+  // if (process.env.NODE_ENV === 'development') {
+  if (typeof window === 'undefined') {
+    (async () => {
+      const { server } = await import('./node');
+      server.listen();
+    })();
+  } else {
+    (async () => {
+      const { worker } = await import('./browser');
+      worker.start();
+    })();
   }
+  // }
 };
 
 export const MSWInitComponent = () => {
