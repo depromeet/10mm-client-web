99f55e5fdafe0568f69eeb53262db20517f1a72a
sumi-0011 <selina2000@naver.com>
2024-01-16 20:57:39 +0900

    ✨ 귀여운 엠엠이 추가

--- src/app/record/success/page.tsx
+++ src/app/record/success/page.tsx
@@ -1,10 +1,12 @@
 'use client';
+
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import lottieJson from '@/assets/lotties/lottieExample.json';
+// import lottieJson from '@/assets/lotties/lottieExample.json';
 import Button from '@/components/Button/Button';
 import { ROUTER } from '@/constants/router';
 import { css } from '@styled-system/css';
-import Lottie from 'react-lottie-player';
+// import Lottie from 'react-lottie-player';
 
 export default function MissionSuccessPage() {
   const router = useRouter();
@@ -14,17 +16,20 @@ export default function MissionSuccessPage() {
     <main className={mainWrapperCss}>
       <div className={containerCss}>
         <div className={contentWrapperCss}>
+          <div className={lottieWrapperCss}>
+            <Image src="/assets/mission/10mm-success.png" alt="success" fill />
+            {/* <Lottie loop animationData={lottieJson} play /> */}
+          </div>
           <div className={titleWrapperCss}>
             <span className={titleCss}>오늘의 미션완료!</span>
             <span className={subTitleCss}>{'잘 하셨어요, 오늘도 한 걸음 성장하셨네요 :)'}</span>
           </div>
-          <div className={lottieWrapperCss}>
-            <Lottie loop animationData={lottieJson} play />
-          </div>
         </div>
-        <Button type="button" size="large" variant="primary" onClick={onClickConfirmButton}>
-          <span className={buttonTextCss}>확인</span>
-        </Button>
+        <div className={buttonWrapperCss}>
+          <Button type="button" size="large" variant="primary" onClick={onClickConfirmButton}>
+            <span className={buttonTextCss}>확인</span>
+          </Button>
+        </div>
       </div>
     </main>
   );
@@ -37,12 +42,16 @@ const mainWrapperCss = css({
   height: '100vh',
 });
 
+const buttonWrapperCss = css({
+  padding: '16px 24px',
+});
+
 const containerCss = css({
   display: 'flex',
   flex: 1,
   flexDirection: 'column',
   justifyContent: 'space-between',
-  padding: '16px 24px',
+  // padding: '16px 24px',
 });
 
 const contentWrapperCss = css({
@@ -50,7 +59,6 @@ const contentWrapperCss = css({
   justifyContent: 'center',
   display: 'flex',
   flexDirection: 'column',
-  gap: '25px',
 });
 
 const titleWrapperCss = css({
@@ -58,6 +66,8 @@ const titleWrapperCss = css({
   flexDirection: 'column',
   textAlign: 'center',
   gap: '4px',
+  position: 'relative',
+  top: '-20px',
 });
 
 const titleCss = css({
@@ -70,7 +80,15 @@ const subTitleCss = css({
   color: 'text.secondary',
 });
 
-const lottieWrapperCss = css({});
+const lottieWrapperCss = css({
+  position: 'relative',
+  minHeight: '260px',
+  maxWidth: '100vw',
+  width: '100%',
+  '& img': {
+    objectFit: 'contain',
+  },
+});
 
 const buttonTextCss = css({
   textStyle: 'subtitle4',
