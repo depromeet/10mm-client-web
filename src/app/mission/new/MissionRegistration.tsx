30341c2536b74cc3e4f91b46e6ef22fdf345923b
sumi-0011 <selina2000@naver.com>
2024-01-16 20:18:10 +0900

    🚑️ 미션 생성 내용 필수 입력 해결

--- src/app/mission/new/MissionRegistration.tsx
+++ src/app/mission/new/MissionRegistration.tsx
@@ -6,10 +6,13 @@ import useCreateMissionMutation from '@/app/mission/new/useCreateMissionMutation
 import Button from '@/components/Button/Button';
 import Input from '@/components/Input/Input';
 import { type DropdownValueType } from '@/components/Input/Input.types';
+import { useSnackBar } from '@/components/SnackBar/SnackBarProvider';
 import { MISSION_CATEGORY_LIST, PUBLIC_SETTING_LIST } from '@/constants/mission';
 import { css } from '@/styled-system/css';
 
 export default function MissionRegistration() {
+  const { triggerSnackBar } = useSnackBar();
+
   const [missionTitleInput, setMissionTitleInput] = useState('');
   const [missionContentInput, setMissionContentInput] = useState('');
   const [missionCategory, setMissionCategory] = useState<DropdownValueType<MissionCategory> | null>(null);
@@ -17,7 +20,8 @@ export default function MissionRegistration() {
     PUBLIC_SETTING_LIST[0],
   );
 
-  const isSubmitButtonDisabled = !missionTitleInput || !missionCategory;
+  const isSubmitButtonDisabled = !missionTitleInput || !missionCategory || !missionContentInput;
+
   const { mutate } = useCreateMissionMutation();
   // 미션 명
   const handleMissionTitleInput = (value: string) => {
@@ -29,7 +33,19 @@ export default function MissionRegistration() {
   };
 
   const handleSubmit = () => {
-    if (!missionCategory) return;
+    if (!missionCategory) {
+      triggerSnackBar({
+        message: '미션 제목을 입력해주세요. ',
+      });
+      return;
+    }
+    if (!missionContentInput) {
+      triggerSnackBar({
+        message: '미션 내용을 입력해주세요. ',
+      });
+      return;
+    }
+
     mutate({
       name: missionTitleInput,
       content: missionContentInput,
@@ -48,16 +64,15 @@ export default function MissionRegistration() {
         maxLength={20}
         value={missionTitleInput}
         onChange={handleMissionTitleInput}
-        description="디스크립션 영역입니다"
       />
       <Input
         type="text"
         placeholder="미션 내용을 입력"
+        required
         name="미션내용"
         maxLength={30}
         value={missionContentInput}
         onChange={handleMissionContentInput}
-        description="디스크립션 영역입니다"
       />
 
       {/* 카테고리 */}
