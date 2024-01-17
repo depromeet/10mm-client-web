4c101df8e7df6ba6396e4d4eae192c2fc7d423b6
Dongmin Ahn <eamon3481@naver.com>
2024-01-17 13:45:03 +0900

    :bug: 타입에러 수정

--- src/app/mypage/Profile.tsx
+++ src/app/mypage/Profile.tsx
@@ -5,9 +5,9 @@ import ProfileContent from '@/app/mypage/ProfileContent';
 
 function Profile() {
   const { data } = useGetMembersMe();
-  if (!data) return <ProfileContent nickname={''} profileUrl={null} />;
-  const { nickname, profileUrl } = data;
-  return <ProfileContent nickname={nickname} profileUrl={profileUrl} />;
+  if (!data) return <ProfileContent nickname={''} />;
+  const { nickname, profileImageUrl } = data;
+  return <ProfileContent nickname={nickname} profileUrl={profileImageUrl} />;
 }
 
 export default Profile;
