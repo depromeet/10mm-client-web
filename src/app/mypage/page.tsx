ca9e16f2bde610978e6d14daf918edeaf47daed6
junoshon <sanoldfox@naver.com>
2024-01-23 02:01:56 +0900

    feat: 헤더 아이콘 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,15 +1,33 @@
 'use client';
+import Link from 'next/link';
 import AppBarBottom from '@/components/AppBarBottom/AppBarBottom';
 import BottomDim from '@/components/BottomDim/BottomDim';
-import Header from '@/components/Header/Header';
+import Icon from '@/components/Icon';
+import { ROUTER } from '@/constants/router';
 import { css } from '@/styled-system/css';
+import { flex } from '@/styled-system/patterns';
 
 import MyProfile from './MyProfile';
+
+function Header() {
+  return (
+    <h2 className={headingCss}>
+      <Link href={ROUTER.MISSION.NEW}>
+        <Icon name="normal-link" size={20} color="icon.primary" />
+      </Link>
+      <Link href={ROUTER.MISSION.NEW}>
+        <Icon name="normal-setting" size={20} color="icon.primary" />
+      </Link>
+      {/* <Icon name={'input-arrow-down'} color={'icon.secondary'} className={iconCss} /> */}
+    </h2>
+  );
+}
 
 function Mypage() {
   return (
     <main className={backgroundCss}>
-      <Header isBackIcon={false} title={'마이페이지'} rightAction="icon" iconName={'menu'} />
+      {/* <Header isBackIcon={false} rightAction="icon" iconName={'menu'} /> */}
+      <Header />
 
       <MyProfile />
       <AppBarBottom />
@@ -21,6 +39,14 @@ const backgroundCss = css({
   height: '100vh',
   display: 'flex',
   flexDirection: 'column',
+});
+const headingCss = flex({
+  padding: '12px 8px',
+  justifyContent: 'flex-end',
+  textStyle: 'body4',
+  color: 'text.primary',
+  userSelect: 'none',
+  gap: '10px',
 });
 
 export default Mypage;
