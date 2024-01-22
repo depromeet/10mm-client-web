965c5876774440fc515a3288d35970ecfa7c8aaf
Dongmin Ahn <eamon3481@naver.com>
2024-01-22 13:26:55 +0900

    chore : follow list mock data 제거

--- src/app/home/ProfileList.tsx
+++ src/app/home/ProfileList.tsx
@@ -4,36 +4,10 @@ import { flex } from '@/styled-system/patterns';
 
 import ProfileItem from './ProfileItem';
 
-const uerProfiles = [
-  {
-    name: '변수미칩',
-    id: 1,
-  },
-  {
-    name: '도모',
-    id: 2,
-  },
-  {
-    name: '당근조이',
-    id: 3,
-  },
-  {
-    name: '당근조이1',
-    id: 4,
-  },
-  {
-    name: '당근조이2',
-    id: 5,
-  },
-  {
-    name: '당근조이3',
-    id: 6,
-  },
-  {
-    name: '당근조이4',
-    id: 7,
-  },
-];
+const uerProfiles: {
+  id: number;
+  name: string;
+}[] = [];
 
 function ProfileList() {
   const { profileId, setProfileId } = useProfileId();
