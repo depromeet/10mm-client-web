f119b4b060c608d231283b7b16bd0433d6cec801
junoshon <sanoldfox@naver.com>
2024-01-24 01:02:19 +0900

    merge: conflict 해결

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,8 +1,33 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
-import { useAuth } from '@/hooks/useAuth';
+const variants = {
+  enter: { opacity: 1, x: 0, zIndex: 1 },
+  exit: (test: boolean) => ({ zIndex: 0, opacity: 0, x: test ? -300 : 300 }),
+};
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  useAuth();
-  return <div>{children}</div>;
+  const [test, setTest] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener('popstate', (_event) => {
+      setTest(false);
+    });
+  }, []);
+
+  return (
+    <motion.main
+      custom={test}
+      variants={variants}
+      exit="exit"
+      animate="enter"
+      transition={{
+        type: 'linear',
+        duration: 0.3,
+      }}
+    >
+      {children}
+    </motion.main>
+  );
 }
