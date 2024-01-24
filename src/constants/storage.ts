6b5bd3dd931a0e352cf45c94e53636d7c5ec5c8c
sumi-0011 <selina2000@naver.com>
2024-01-24 22:46:16 +0900

    refactor: 이전 storage key 제거

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,11 +1,4 @@
 export const STORAGE_KEY = {
-  // TODO : 삭제
-  STOPWATCH: {
-    TIME: 'stopwatch-time', // 웹 이탈 시 체크
-    TIME_2: 'stopwatch-time-2', // 테스트 용
-    START_TIME: 'stopwatch-start-time',
-    MISSION_ID: 'stopwatch-mission-id',
-  },
   PROGRESS_MISSION: {
     MISSION: 'progress-mission-info',
     TIME: (missionId: string) => `progress-mission-${missionId}-time`,
