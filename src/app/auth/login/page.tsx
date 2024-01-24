9787865886c31ee47221c6de28a11d4c4952965a
wooBottle <dkrnfls@gmail.com>
2024-01-24 23:00:20 +0900

    아이디로 로그인 임시 추가

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -64,6 +64,10 @@ export default function LoginPage() {
     });
   };
 
+  const onClickLoginButton = () => {
+    router.push(ROUTER.AUTH.SIGNIN);
+  };
+
   useEffect(() => {
     const appleIdSignInOnSuccessHandler = (event: CustomEvent) => {
       mutateAsync(
@@ -127,6 +131,9 @@ export default function LoginPage() {
       </head>
       <div className={MainWrapperCss}>
         <div className={LoginButtonListWrapperCss}>
+          <Button variant="primary" size="large" onClick={onClickLoginButton}>
+            아이디로 로그인
+          </Button>
           {isIOS() && <ButtonSocialLogin type="apple" onClick={onClickAppleLogin} />}
           <ButtonSocialLogin type="kakao" onClick={onClickKakaoLogin} />
           <Button size="large" variant="ghost" onClick={onClickGuest}>
