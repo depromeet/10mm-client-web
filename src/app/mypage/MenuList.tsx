fbac4deb30deba2fac5d3aba08844e63243ba9bc
Dongmin Ahn <eamon3481@naver.com>
2024-01-26 00:18:32 +0900

    chore : 로그인 정보 메뉴 주석처리

--- src/app/mypage/MenuList.tsx
+++ src/app/mypage/MenuList.tsx
@@ -19,7 +19,7 @@ function MenuList() {
     <main className={mainCss}>
       <ul className={listCss}>
         <MenuItem name="이용약관" iconName="terms" url={ROUTER.MYPAGE.TERMS} />
-        <MenuItem name="로그인정보" iconName="log-in-information" url={ROUTER.MYPAGE.LOGIN_INFO} />
+        {/*<MenuItem name="로그인정보" iconName="log-in-information" url={ROUTER.MYPAGE.LOGIN_INFO} />*/}
         <MenuItem name="로그아웃" iconName="log-out" url="#" onClick={onLogoutClick} />
         <MenuItem name="회원탈퇴" iconName="withdrawal" url={ROUTER.MYPAGE.WITHDRAWAL} />
       </ul>
