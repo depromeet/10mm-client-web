b5209fe04ac9e8a45e680f19dde3ff1ff33c1a18
sumi-0011 <selina2000@naver.com>
2024-01-29 17:40:38 +0900

    refactor: motion div 수정 및 fade up 모션 추가

--- src/components/Motion/MotionDiv.tsx
+++ src/components/Motion/MotionDiv.tsx
@@ -1,15 +1,28 @@
 'use client';
 
-import { type ComponentProps } from 'react';
+import { type ComponentProps, useMemo } from 'react';
 import { motion, type Variants } from 'framer-motion';
 
-interface Props extends ComponentProps<typeof motion.div> {}
+interface Props extends Omit<ComponentProps<typeof motion.div>, 'variants'> {
+  variants?: Variants | 'fadeIn' | 'fadeInUp';
+}
 
-function MotionDiv(props: Props) {
+function MotionDiv({ variants = 'fadeIn', ...props }: Props) {
+  const _variants: Variants = useMemo(() => {
+    switch (variants) {
+      case 'fadeIn':
+        return defaultFadeInVariants;
+      case 'fadeInUp':
+        return defaultFadeInUpVariants;
+      default:
+        return variants;
+    }
+  }, [variants]);
+
   return (
     <motion.div
       {...props}
-      variants={props.variants ?? defaultFadeInVariants}
+      variants={_variants}
       initial={props.initial ?? 'initial'}
       animate={props.animate ?? 'animate'}
       exit={props.exit ?? 'exit'}
@@ -36,5 +49,26 @@ const defaultFadeInVariants: Variants = {
     opacity: 0,
     transition: { duration: 0.3, ease: defaultEasing },
     willChange: 'opacity',
+  },
+};
+
+const defaultFadeInUpVariants: Variants = {
+  initial: {
+    opacity: 0,
+    y: 10,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity, transform',
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity, transform',
+  },
+  exit: {
+    opacity: 0,
+    y: 10,
+    transition: { duration: 0.3, ease: defaultEasing },
+    willChange: 'opacity, transform',
   },
 };
