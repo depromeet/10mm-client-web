c6a799ba9d8488353cfd1f75932b7a754e4b004d
Dongmin Ahn <eamon3481@naver.com>
2024-02-05 00:26:27 +0900

    chore : 안쓰는 코드 제거

--- src/app/mission/[id]/detail/MissionCalender/MissionCalendar.utils.ts
+++ src/app/mission/[id]/detail/MissionCalender/MissionCalendar.utils.ts
@@ -2,58 +2,6 @@ import { type RecordType } from '@/apis/schema/record';
 import { ROUTER } from '@/constants/router';
 import dayjs, { type Dayjs } from 'dayjs';
 
-const getWeekArray = (totalDate: number, offsetDate: number) => {
-  return Array.from({ length: 7 }, (_, i) => {
-    const currentDate = offsetDate + i;
-    if (currentDate < 0) {
-      return 0;
-    }
-
-    if (currentDate > totalDate) {
-      return 0;
-    }
-    return currentDate;
-  });
-};
-
-const getMonthArray = (prevBlankCount: number, totalDate: number) => {
-  const weekNumber = Math.ceil((prevBlankCount + totalDate) / 7);
-
-  return Array.from({ length: weekNumber }, (_, weekIndex) => {
-    const offsetDate = weekIndex * 7 - prevBlankCount + 1;
-
-    return getWeekArray(totalDate, offsetDate);
-  });
-};
-
-export const getCalenderInfo = (currentMonth: number, currentYear: number) => {
-  // 이번달 1일
-  const firstDate = new Date(currentYear, currentMonth - 1, 1);
-
-  // 이번달 마지막날
-  const lastDate = new Date(currentYear, currentMonth, 0);
-
-  // getDay() : 일요일 0 ~ 토요일 6
-  const prevBlankCount = firstDate.getDay();
-  const totalDate = lastDate.getDate();
-
-  const monthCalendarData = getMonthArray(prevBlankCount, totalDate).map((week) =>
-    week.map((date) => {
-      if (date === 0) {
-        return null;
-      }
-
-      return {
-        year: currentYear,
-        month: currentMonth,
-        date,
-      };
-    }),
-  );
-
-  return { monthCalendarData };
-};
-
 export const getMissionCalendarItemProps = (
   missionStartAt: string,
   day: {
