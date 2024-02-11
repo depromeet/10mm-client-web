d9e66198711b56efedc61cf5b630f043cb9cdfad
Dongmin Ahn <eamon3481@naver.com>
2024-02-11 23:17:59 +0900

    fix : 피드탭, 프로필 피드탭 empty 상태 화면 추가

--- src/app/feed/FeedList.tsx
+++ src/app/feed/FeedList.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useFeedMe } from '@/apis/feed';
 import FeedItem, { FeedSkeletonItem } from '@/app/feed/FeedItem';
+import Empty from '@/components/Empty/Empty';
+import { ROUTER } from '@/constants/router';
 import { css } from '@styled-system/css';
 
 function FeedList() {
@@ -13,6 +15,21 @@ function FeedList() {
       </ul>
     );
 
+  if (data.length === 0) {
+    return (
+      <div className={emptyFeedCss}>
+        <Empty
+          type={'suggest'}
+          image={'docs'}
+          link={ROUTER.SEARCH.HOME}
+          title={'피드가 없습니다.'}
+          buttonText={'친구 찾기'}
+          description={'친구를 팔로우하고 피드를 받아볼까요?'}
+        />
+      </div>
+    );
+  }
+
   return (
     <ul className={feedListCss}>
       {data.map((feed) => (
@@ -23,6 +40,13 @@ function FeedList() {
 }
 
 export default FeedList;
+
+const emptyFeedCss = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: 'calc(100vh - 200px)',
+});
 
 const feedListCss = css({
   padding: '0 16px 132px 16px',
