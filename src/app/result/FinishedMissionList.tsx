ce57233df528f87baa1eb4238259c9050047c3f9
sumi-0011 <selina2000@naver.com>
2024-02-14 20:34:14 +0900

    feat: 종료미션 빈 화면 추가

--- src/app/result/FinishedMissionList.tsx
+++ src/app/result/FinishedMissionList.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 import { type GetFinishedMissionsResponse } from '@/apis/mission';
 import { type MissionCategory } from '@/apis/schema/mission';
 import Badge from '@/components/Badge/Badge';
+import Empty from '@/components/Empty/Empty';
 import { TwoLineListItem } from '@/components/ListItem';
 import { MISSION_CATEGORY_LABEL } from '@/constants/mission';
 import { ROUTER } from '@/constants/router';
-import { flex } from '@/styled-system/patterns';
+import { center, flex } from '@/styled-system/patterns';
 import { type UseQueryResult } from '@tanstack/react-query';
 
 interface Props {
@@ -13,9 +14,22 @@ interface Props {
 }
 
 function FinishedMissionList({ queryData }: Props) {
-  const { data } = queryData;
+  const { data, isLoading } = queryData;
 
   const list = data ?? [];
+
+  if (!isLoading && list.length === 0) {
+    return (
+      <div className={emptyContainerCss}>
+        <Empty
+          type="notice"
+          title="아직 종료된 미션이 없어요."
+          description="미션을 등록하고 14일이 지나면 <br/>종료 미션으로 등록됩니다."
+          image={'docs'}
+        />
+      </div>
+    );
+  }
 
   return (
     <ul className={ulCss}>
@@ -47,4 +61,16 @@ const ulCss = flex({
   padding: '16px',
   gap: '8px',
   flexDirection: 'column',
+});
+
+const emptyContainerCss = center({
+  height: 'calc(100vh - 61px)',
+
+  '& > article': {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
