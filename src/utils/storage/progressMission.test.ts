49ac48a1c189864ecf3ae4891acad3449327ee8d
sumi-0011 <selina2000@naver.com>
2024-02-19 18:57:38 +0900

    test: progress mission test 추가 및 기존 코드 리팩토링

---
+++ src/utils/storage/progressMission.test.ts
@@ -0,0 +1,24 @@
+import { STORAGE_KEY } from '@/constants/storage';
+import { setMissionTimeStack, setProgressMissionTime } from '@/utils/storage/progressMission';
+
+const TEST_MISSION_ID = '0';
+test('1 is 1', () => {
+  expect(1).toBe(1);
+});
+
+// setProgressMissionTime test
+test('setProgressMissionTime test', () => {
+  setProgressMissionTime(TEST_MISSION_ID, 1000);
+  expect(localStorage.getItem(STORAGE_KEY.PROGRESS_MISSION.TIME(TEST_MISSION_ID))).toBe('1000');
+});
+
+// setMissionTimeStack
+test('setMissionTimeStack test', () => {
+  const time = new Date().getTime();
+  const timeInfo = { time, status: 'start' };
+
+  setMissionTimeStack(TEST_MISSION_ID, 'start');
+  expect(localStorage.getItem(STORAGE_KEY.PROGRESS_MISSION.TIME_STACK(TEST_MISSION_ID))).toBe(
+    JSON.stringify([timeInfo]),
+  );
+});
