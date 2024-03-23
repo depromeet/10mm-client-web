c75c822df1ef97cc761b775aa5b97bb21defe066
Dongmin Ahn <eamon3481@naver.com>
2024-03-23 11:37:34 +0900

    feat : real 버전 추천 팔로우 업데이트

--- src/app/onboarding/onboarding.constants.ts
+++ src/app/onboarding/onboarding.constants.ts
@@ -21,9 +21,45 @@ export const RECOMMENDATION = [
 
 export const RECOMMENDATION_REAL = [
   {
+    id: 4,
+    nickname: '도모',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/4/38e6a5a9-a547-4051-95ea-cc112feabe21.jpeg',
+    tags: ['#기타연주', '#감사일기', '#개발자'],
+  },
+  {
+    id: 2,
+    nickname: 'ybchar',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/2/2443459a-94b5-4048-98df-5fe356378a62.jpeg',
+    tags: ['#개발', '#산책하기'],
+  },
+  {
+    id: 73,
+    nickname: '안암위스키남',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/73/a362d028-ad4c-49aa-9912-e32bc041793d.jpeg',
+    tags: ['#공부', '#헬스', '#디자인'],
+  },
+  {
     id: 1,
-    nickname: '수미미칩',
-    profileImageUrl: null,
-    tags: ['tag1', 'tag2', 'tag3'],
+    nickname: '우보틀',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/1/b83aeeb8-9acd-4915-aee4-ea87ec142b3b.jpeg',
+    tags: ['#카페출근', '#출근독서', '#스타벅스'],
+  },
+  {
+    id: 8,
+    nickname: '수미칩',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/8/70d5cb9b-95ff-406f-93bd-9a68a2d8d5b9.png',
+    tags: ['#운동', '#뜨개질', '#폭주기관차'],
+  },
+  {
+    id: 7,
+    nickname: '유우비트',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/7/9fbdf25a-101e-4fb5-9956-6030e7407382.jpeg',
+    tags: ['#운동', '#헬스', '#영화'],
+  },
+  {
+    id: 9,
+    nickname: '집가고시퍼',
+    profileImageUrl: 'https://image.10mm.today/prod/member_profile/9/722d875d-eff0-498d-9644-e1c8eb514414.jpeg',
+    tags: ['#디자인', '#회의'],
   },
 ];
