12731f934978cb88b6815d83e93a280d967c97f7
Dongmin Ahn <eamon3481@naver.com>
2024-03-23 10:06:57 +0900

    feat : OnboardingPage 페이지 상단 레이아웃 구현

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,51 @@
+import Button from '@/components/Button/Button';
+import CenterTextHeader from '@/components/Header/CenterTextHeader';
+import { css } from '@styled-system/css';
+
+function OnboardingPage() {
+  return (
+    <div>
+      <CenterTextHeader
+        title={'추천 친구'}
+        rightComponent={
+          <Button variant={'ghost'} size={'medium'}>
+            건너뛰기
+          </Button>
+        }
+      />
+      <div className={textSectionCss}>
+        <img className={assetImagCss} src={'/assets/character/flag.png'} alt={'10mm character flag'} />
+        <p className={titleCss}>친구를 팔로우 해보세요!</p>
+        <p className={subTitleCss}>팔로우를 통해 미션과 인증을 공유할 수 있어요.</p>
+      </div>
+    </div>
+  );
+}
+
+export default OnboardingPage;
+
+const assetImagCss = css({
+  width: '112px',
+  height: '84px',
+});
+
+const titleCss = css({
+  textStyle: 'title3',
+  color: 'text.primary',
+  marginBottom: '4px',
+});
+
+const subTitleCss = css({
+  textStyle: 'body4',
+  color: 'gray.gray600',
+});
+
+const textSectionCss = css({
+  paddingTop: '28px',
+  paddingBottom: '56px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
